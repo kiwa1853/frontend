0ea861574efe719227b68dba19426d2f57470163
kiwa1853 <gimg26746@naver.com>
2024-12-13 15:22:02 +0900

    커밋

--- src/Page4.js
+++ src/Page4.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './PageComponents/Header'
 import MainHeader from './Page2Components/MainHeader'
 import Page4Main from './Page4Components/MainData';
-import Juso from './Page4Components/Juso';
+import Juso from './Page4Components/GwangGo';
  function App() {
   return (
     <div className="App">
